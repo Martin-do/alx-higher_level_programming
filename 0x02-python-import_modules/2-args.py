21d2d79a10513ec9eb3f16248ecba8af62c68b88
Martin-do <martinajaiyeola40@gmail.com>
2023-08-10 15:32:08 -0700

    "refactored some files"

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 
-
 if __name__ == "__main__":
     from sys import argv
-    
+
     arg_count = len(argv) - 1
     if arg_count == 0:
         response = "arguments."
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     from sys import argv
-    
+
     arg_count = len(argv) - 1
     sum = 0
     if arg_count != 0:
