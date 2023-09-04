31c9ba43e21898714adbd6c2862e1ad640ed7771
Martin-do <martinajaiyeola40@gmail.com>
2023-09-04 13:59:49 -0700

    "refactored file for task 9"

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -128,9 +128,3 @@ class Rectangle:
 
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
-
-
-if __name__ == '__main__':
-    my_square = Rectangle.square(5)
-    print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-    print(my_square)
