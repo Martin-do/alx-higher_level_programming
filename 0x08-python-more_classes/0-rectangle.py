5f5cf614c2c5033af8b25dfaea9402af2360479a
Martin-do <martinajaiyeola40@gmail.com>
2023-09-04 13:15:19 -0700

    "refactored files for task 0"

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,10 +3,5 @@
 
 
 class Rectangle:
+    """empty rectangle class"""
     pass
-
-
-if __name__ == "__main__":
-    my_rectangle = Rectangle()
-    print(type(my_rectangle))
-    print(my_rectangle.__dict__)
