5484b166e4f9f5874225e2e121262a7265829e49
Martin-do <martinajaiyeola40@gmail.com>
2023-09-04 13:39:22 -0700

    "edited files for task 3 and 4"

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -77,7 +77,7 @@ class Rectangle:
         string = ""
         if self.__width == 0 or self.__height == 0:
             return string
-        for i in range(self.__height):
+        for i in range(self.__height - 1):
             for j in range(self.__width):
                 string += "#"
             string += "\n"
