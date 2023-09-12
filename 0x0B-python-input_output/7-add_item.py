61bb93f1b0c363ef4a4d174a567e0ed1281171f0
Martin-do <martinajaiyeola40@gmail.com>
2023-09-12 06:30:10 -0700

    "refactored some files"

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,20 +10,14 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-def main():
-    """this is the main driver functin"""
-    f_name = "add_items.json"
-    args = len(sys.argv)
+f_name = "add_items.json"
+args = len(sys.argv)
+if not os.path.isfile(f_name):
+    with open(f_name, 'w', encoding='utf-8') as f:
+        f.write('[]')
 
-    if not os.path.isfile(f_name):
-        with open(f_name, 'w', encoding='utf-8') as f:
-            f.write('[]')
-
-    if args > 1:
-        data = load_from_json_file(f_name)
-        for arg in range(1, len(sys.argv)):
-            data.append(sys.argv[arg])
-        save_to_json_file(data, f_name)
-
-
-main()
+if args > 1:
+    data = load_from_json_file(f_name)
+for arg in range(1, len(sys.argv)):
+    data.append(sys.argv[arg])
+save_to_json_file(data, f_name)
