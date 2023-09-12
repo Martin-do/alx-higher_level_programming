848ad76437b8d5d94a91e775727fba788eea882d
Martin-do <martinajaiyeola40@gmail.com>
2023-09-12 06:02:38 -0700

    "edited files fortask 0"

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,3 +12,7 @@ def read_file(filename=""):
     with open(filename, 'r', encoding="UTF-8") as f:
         read_data = f.read()
     print(read_data)
+
+
+if __name__ == '__main__':
+    read_file("my_file_0.txt")
